cd78650cb654fe33d93302e1531457b19735e21d
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-08 17:43:25 +0100

    creazione AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,60 @@ export default {
 </script>
 
 <template>
-    <h1>Header e Nav</h1>
+    <div class="container">
+        <img src="../assets/img/dc-logo.png" alt="">
+
+        <ul>
+            <li>
+                <a href="">COMICS</a>
+            </li>
+            <li>
+                <a href="">COMICS</a>
+            </li>
+            <li>
+                <a href="">COMICS</a>
+            </li>
+            <li>
+                <a href="">COMICS</a>
+            </li>
+            <li>
+                <a href="">COMICS</a>
+            </li>
+            <li>
+                <a href="">COMICS</a>
+            </li>
+            <li>
+                <a href="">COMICS</a>
+            </li>
+            <li>
+                <a href="">COMICS</a>
+            </li>
+            <li>
+                <a href="">COMICS</a>
+            </li>
+            <li>
+                <a href="">COMICS</a>
+            </li>
+        </ul>
+    </div>
+
 </template>
 
 <style lang="scss" scoped>
+.container {
+    width: 70%;
+    margin: 20px auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
 
+ul {
+    display: flex;
+}
+
+li {
+    margin: 0 1rem;
+    font-weight: bolder;
+}
 </style>
